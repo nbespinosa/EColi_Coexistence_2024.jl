9ac05653fd61db8342eeb86ce3877a52c46dfaa1
josePereiro <jose.pereiro.personal@gmail.com>
2024-07-11 18:43:32 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.48

---
+++ src/EColi_Coexistence_2024.jl
@@ -0,0 +1,5 @@
+module EColi_Coexistence_2024
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using EColi_Coexistence_2024
+using Test
+
+@testset "EColi_Coexistence_2024.jl" begin
+    # Write your tests here.
+end
